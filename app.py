92581fc6ff2105a2cf4f065383e1d423a58bd26c
veera <veerababu.narni@gmail.com>
2026-09-09 08:12:37 +0530

    app.py

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
 # Check the voting eligibility condition
+
 if age >= 100:
     print(f"Hello {name}! You are eligible to vote.")
-else:
-    years_left = 50 - age
+
+if age >= 10:
+    print(f"Hello {name}! You are eligible t5t vote.")
+
+    years_left = 5000 - agq
     print(f"Hello {name}. You will be eligible to vote in {years_left} years.")
