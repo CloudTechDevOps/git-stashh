02193397f403bfe001b601d5e0c1065769ac7484
veera <veerababu.narni@gmail.com>
2026-09-09 08:03:25 +0530

    app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
 # Check the voting eligibility condition
-if age >= 18:
+if age >= 28:
     print(f"Hello {name}! You are eligible to vote.")
 else:
-    years_left = 18 - age
+    years_left = 28 - age
     print(f"Hello {name}. You will be eligible to vote in {years_left} years.")
